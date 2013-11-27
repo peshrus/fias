7a0c922b3b27117831513d8198869761b7266e65
peshrus <peshrus@gmail.com>
2013-11-27 18:13:28 +0400

    FIAS XML Saver: reading of files from archive and printing of entities to console has been implemented

--- xml-saver/src/main/java/com/peshchuk/fias/xml/saver/FiasXmlSaver.java
+++ xml-saver/src/main/java/com/peshchuk/fias/xml/saver/FiasXmlSaver.java
@@ -3,15 +3,25 @@ package com.peshchuk.fias.xml.saver;
 import com.github.junrar.Archive;
 import com.github.junrar.exception.RarException;
 import com.github.junrar.rarfile.FileHeader;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Ruslan Peshchuk (peshrus@gmail.com)
@@ -19,43 +29,92 @@ import java.io.InputStream;
 public class FiasXmlSaver {
 	private static final Logger LOGGER = LoggerFactory.getLogger(FiasXmlSaver.class);
 
+	/**
+	 * See jaxb2-maven-plugin configuration in pom.xml
+	 */
+	private static final String JAXB_CLASSES_PACKAGE = "com.peshchuk.fias.jaxb";
+
 	private final File fiasRarFile;
 	private final Unmarshaller jaxbUnmarshaller;
 
 	public FiasXmlSaver(File fiasRarFile) throws JAXBException {
 		this.fiasRarFile = fiasRarFile;
 
-		final JAXBContext jaxbContext = JAXBContext.newInstance("com.peshchuk.fias.jaxb");
+		final JAXBContext jaxbContext = JAXBContext.newInstance(JAXB_CLASSES_PACKAGE);
 		jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 	}
 
-	public void save() throws IOException, RarException, JAXBException {
+	public void save() throws IOException, RarException, JAXBException, ClassNotFoundException {
+		final Set<Class<?>> jaxbClasses = getJaxbClasses();
+		final Set<String> xmlRootElements = new HashSet<>();
+		final Map<String, Class<?>> entityClasses = new HashMap<>();
+
+		for (Class<?> jaxbClass : jaxbClasses) {
+			final String xmlRootElement = jaxbClass.getAnnotation(XmlRootElement.class).name();
+			xmlRootElements.add(xmlRootElement);
+
+			for (Class<?> entityClass : jaxbClass.getClasses()) {
+				entityClasses.put(entityClass.getSimpleName(), entityClass);
+			}
+		}
+
 		LOGGER.info("Start Saving: {}", fiasRarFile);
 		try {
 			final Archive archive = new Archive(fiasRarFile);
 
 			if (!archive.isEncrypted()) {
+				final XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
 				FileHeader fileHeader;
 				while ((fileHeader = archive.nextFileHeader()) != null) {
 					if (!fileHeader.isEncrypted()) {
-						LOGGER.info("Reading: {}", fileHeader.getFileNameString());
 						if (fileHeader.isFileHeader()) {
+							LOGGER.info("Reading: {}", fileHeader.getFileNameString());
+
 							try (final InputStream inputStream = archive.getInputStream(fileHeader)) {
-								Object unmarshal = jaxbUnmarshaller.unmarshal(inputStream);
-								System.out.println(unmarshal);
+								final XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(inputStream);
+								try {
+									saveFile(xmlRootElements, entityClasses, xmlStreamReader);
+								} finally {
+									xmlStreamReader.close();
+								}
+							} catch (XMLStreamException e) {
+								LOGGER.error(e.toString(), e);
 							}
 						} else {
-							LOGGER.error("File cannot been read: {}", fileHeader.getFileNameString());
+							LOGGER.error("File cannot be read: {}", fileHeader.getFileNameString());
 						}
 					} else {
-						LOGGER.error("File is encrypted cannot read: {}", fileHeader.getFileNameString());
+						LOGGER.error("File is encrypted: {}", fileHeader.getFileNameString());
 					}
 				}
 			} else {
-				LOGGER.error("Archive is encrypted cannot read: {}", fiasRarFile);
+				LOGGER.error("Archive is encrypted: {}", fiasRarFile);
 			}
 		} finally {
 			LOGGER.info("Finish Saving: {}", fiasRarFile);
 		}
+	}
+
+	private Set<Class<?>> getJaxbClasses() {
+		final Reflections reflections = new Reflections(JAXB_CLASSES_PACKAGE);
+		final Set<Class<?>> result = reflections.getTypesAnnotatedWith(XmlRootElement.class);
+
+		return result;
+	}
+
+	private void saveFile(Set<String> xmlRootElements, Map<String, Class<?>> entityClasses, XMLStreamReader xmlStreamReader) throws XMLStreamException, JAXBException {
+		do {
+			xmlStreamReader.nextTag();
+		} while (xmlRootElements.contains(xmlStreamReader.getLocalName()));
+
+		do {
+			final Class<?> entityClass = entityClasses.get(xmlStreamReader.getLocalName());
+			saveEntity(xmlStreamReader, entityClass);
+		} while (xmlStreamReader.isStartElement() && xmlStreamReader.hasNext());
+	}
+
+	private <T> void saveEntity(XMLStreamReader xmlStreamReader, Class<T> entityClass) throws JAXBException {
+		final JAXBElement<T> jaxbElement = jaxbUnmarshaller.unmarshal(xmlStreamReader, entityClass);
+		System.out.println(jaxbElement.getValue());
 	}
 }
--- xml-saver/src/test/java/com/peshchuk/fias/xml/saver/FiasXmlSaverTest.java
+++ xml-saver/src/test/java/com/peshchuk/fias/xml/saver/FiasXmlSaverTest.java
@@ -1,12 +1,10 @@
 package com.peshchuk.fias.xml.saver;
 
-import com.github.junrar.exception.RarException;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBException;
 import java.io.File;
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -21,13 +19,13 @@ public class FiasXmlSaverTest {
 
 	@Before
 	public void setUp() throws URISyntaxException, JAXBException {
-		final URL fiasRarUrl = FiasXmlSaverTest.class.getClassLoader().getResource("fias_xml.rar");
+		final URL fiasRarUrl = FiasXmlSaverTest.class.getClassLoader().getResource("fias_delta_xml.rar");
 		assertNotNull(fiasRarUrl);
 		saver = new FiasXmlSaver(new File(new URI(fiasRarUrl.toString()).getPath()));
 	}
 
 	@Test
-	public void testSave_Ok() throws IOException, RarException, JAXBException {
+	public void testSave_Ok() throws Exception {
 		saver.save();
 	}
 }
